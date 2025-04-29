0bf57e079049b9160a7f16ecd58fd776c0bbc502
Hitesh Borse <nskhitesh2016@gmail.com>
2025-04-29 20:07:42 +0530

    cors bug fixed

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ const tenderRoutes = require("./routes/tenders");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://tendor-project-l7t2.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "https://tendor-project-l7t2.vercel.app/",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
+
 app.use(express.json());
 
 app.use("/api/tenders", tenderRoutes);
