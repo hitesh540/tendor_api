15d65dab44cbdf1db9a0d82fd4a755a6d126b96c
Hitesh Borse <nskhitesh2016@gmail.com>
2025-04-29 18:10:40 +0530

    env updates

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const tenderRoutes = require("./routes/tenders");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tendor-project-l7t2.vercel.app/",
+  })
+);
 app.use(express.json());
 
 app.use("/api/tenders", tenderRoutes);
